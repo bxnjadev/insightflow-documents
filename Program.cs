f2f5b28000dc26b32931ab5341577101a3f2e2df
bxnjadev <benjamin.hyx@gmail.com>
2025-12-14 19:44:08 -0300

    feat: added cors configuration

--- Program.cs
+++ Program.cs
@@ -2,12 +2,25 @@ using insightflow_documents.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddOpenApi();
 
 builder.Services.AddSingleton<IDocumentService, DocumentService>();
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 if (app.Environment.IsDevelopment())
 {
